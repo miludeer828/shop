b8f696cf80d28255fd391def7465b054e0ec8db1
“miludeer828” <2574036286@qq.com>
2024-08-09 17:19:11 +0800

    hello

---
+++ remove_http.py
@@ -0,0 +1,4 @@
+nostarch_url = "https://nostarch.com"
+nostarch_url = nostarch_url.removeprefix("https://")
+print(nostarch_url)
+#删除前缀
---
+++ tab.py
@@ -0,0 +1,6 @@
+print("Python")
+print("\tPYthon")
+#\t表示制表符
+
+print("languages:\nPython\nC\nJavascript")
+#\n表示换行符
